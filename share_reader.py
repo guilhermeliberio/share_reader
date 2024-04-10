b59ad7ef2fba299142d81e4f8a8c69a84237ca76
Guilherme Liberio <158169927+guilhermeliberio@users.noreply.github.com>
2024-04-10 13:21:14 -0300

    share reader initial commit

---
+++ share_reader.py
@@ -0,0 +1,31 @@
+import os
+import csv
+import pprint as pp
+
+share_dict = {}
+files = [os.path.join('Shares\\FIN_CORP_RESTR\\', 'FIN_CORP_RESTR_CURATED.txt'), os.path.join('Shares\\FIN_CORP_RESTR\\', 'FIN_CORP_RESTR_RAW.txt'),
+         os.path.join('Shares\\FIN_FL_RESTR\\', 'FIN_FL_RESTR_CURATED.txt'), os.path.join('Shares\\FIN_FL_RESTR\\', 'FIN_FL_RESTR_RAW.txt'),
+         os.path.join('Shares\\FIN_GENERAL\\', 'FIN_GENERAL_CURATED.txt'), os.path.join('Shares\\FIN_GENERAL\\', 'FIN_GENERAL_RAW.txt'),
+         os.path.join('Shares\\FIN_UPST_RESTR\\', 'FIN_UPST_RESTR_CURATED.txt'), os.path.join('Shares\\FIN_UPST_RESTR\\', 'FIN_UPST_RESTR_RAW.txt')]
+
+for file in files:
+    f = open(file, 'r')
+    for line in f.readlines():
+        if "share" in line and "--" not in line and line[0] != "!":
+            splits = line.split(" to share ")
+            key = splits[-1].strip()[0:-1]
+            value = splits[0].strip().split(" ")[-1].strip('""')
+            if "SHARE" in value:
+                if key in share_dict:
+                    share_dict[key].append(value)
+                else:
+                    share_dict[key] = [value]
+
+
+with open('output.csv', 'w') as output:
+    writer = csv.writer(output)
+    for key, value in share_dict.items():
+        writer.writerow([key, value, len(value)])
+
+pp.pprint(share_dict)
+    
