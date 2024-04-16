1dcfc7c9d7fa8cf48375b289e7101f43c2a6507f
Guilherme Liberio <158169927+guilhermeliberio@users.noreply.github.com>
2024-04-16 10:41:00 -0300

    dealing with sources at the end of the name and conditional logic to exclude duplicates

--- share_reader.py
+++ share_reader.py
@@ -1,6 +1,6 @@
 import os
 import csv
-# import pprint as pp
+import pprint as pp
 
 share_dict = {}
 files = [os.path.join('Shares\\FIN_CORP_RESTR\\', 'FIN_CORP_RESTR_CURATED.txt'), os.path.join('Shares\\FIN_CORP_RESTR\\', 'FIN_CORP_RESTR_RAW.txt'),
@@ -10,22 +10,26 @@ files = [os.path.join('Shares\\FIN_CORP_RESTR\\', 'FIN_CORP_RESTR_CURATED.txt'),
 
 for file in files:
     f = open(file, 'r')
+    count = 1
     for line in f.readlines():
         if "share" in line and "--" not in line and line[0] != "!":
             splits = line.split(" to share ")
             key = splits[-1].strip()[0:-1]
             value = splits[0].strip().split(" ")[-1].strip('""')
             if "VWS" in value:
-                if "_EU_" in value: value.replace("_EU_", "")
-                if "_AP_" in value: value.replace("_AP_", "")
-                if "_AM_" in value: value.replace("_AM_", "")
-                if key in share_dict and value not in share_dict[key]: share_dict[key].append(value)
-                else: share_dict[key] = [value]
-
+                if "_EU_" in value: value = value.replace("_EU_", "")
+                elif "_AP_" in value: value = value.replace("_AP_", "")
+                elif "_AM_" in value: value = value.replace("_AM_", "")
+                else:
+                    last_split = value.split("_")[-1]
+                    if last_split in "EUAPAMG3S8XTID":
+                        value = value.replace("_" + last_split, "")
+                if key not in share_dict: share_dict[key] = [value]
+                elif value not in share_dict[key]: share_dict[key].append(value)
+        
 with open('output.csv', 'w') as output:
     writer = csv.writer(output)
     for key, value in share_dict.items():
         writer.writerow([key, value, len(value)])
 
-# pp.pprint(share_dict)
     
